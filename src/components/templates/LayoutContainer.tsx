03a3cbdaaedbbe4b98c45759e9db859f414a2fa3
Alex Ricardo Castañeda Rodríguez <castaneda.systems@gmail.com>
2026-01-11 17:09:29 -0600

    feat(LayoutContainer): add PostAuthRedirect component for post-authentication handling

--- src/components/templates/LayoutContainer.tsx
+++ src/components/templates/LayoutContainer.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "@organisms/Header";
 import StarsCanvas from "@components/atoms/StarBackground";
 import Footer from "@components/organisms/Footer";
+import { PostAuthRedirect } from "@components/templates/PostAuthRedirect";
 
 const LayoutContainer: React.FC = () => {
   return (
     <>
       <StarsCanvas />
+      <PostAuthRedirect />
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="relative z-0 flex-grow">
